058d7124da085f7b66013a238fc88f9b47a9e041
niCognition <Niclas.p1990@gmail.com>
2020-07-31 15:39:20 +0200

    recap of part I exercise

---
+++ Part 1/Part08_04.LiquidContainers2/LiquidContainers2.java
@@ -0,0 +1,46 @@
+
+import java.util.Scanner;
+
+public class LiquidContainers2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Container first = new Container();
+        Container second = new Container();
+
+        while (true) {
+            System.out.println("First: " + first);
+            System.out.println("Second: " + second);
+            System.out.print("> ");
+
+            String input = scan.nextLine();
+            if (input.equals("quit")) {
+                break;
+            }
+
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.parseInt(parts[1]);
+
+            if (command.equals("add")) {
+                first.add(amount);
+            }
+
+            if (command.equals("move")) {
+                if (first.contains() < amount) {
+                    second.add(first.contains());
+                    first.remove(amount);
+                } else {
+                    first.remove(amount);
+                    second.add(amount);
+                }
+            }
+
+            if (command.equals("remove")) {
+                second.remove(amount);
+            }
+        }
+    }
+
+}
